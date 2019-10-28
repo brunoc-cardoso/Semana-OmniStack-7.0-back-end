1a552089996a358322d60fd9838e6e6393858d9e
bruno <brunocpessoal@gmail.com>
2019-10-28 21:23:33 +0000

    added image upload and added posts registration route

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const multer = require('multer');
+const uploadConfig = require('./config/upload');
+const PostController = require('./controllers/PostController');
 
 const routes =  new express.Router();
+const upload = multer(uploadConfig);
 
 routes.get('/', (req, res) => {
   return res.status(200).json({ message: 'Server is up...'});
 });
+
+routes.get('/posts', PostController.index);
+routes.post('/posts', upload.single('image'), PostController.store);
 
 module.exports = routes;
