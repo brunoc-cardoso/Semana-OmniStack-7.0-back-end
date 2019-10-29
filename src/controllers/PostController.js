e028867d55cf9b904b0bc865b845317369995055
bruno <brunocpessoal@gmail.com>
2019-10-29 12:45:59 +0000

    added post listing route

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -2,7 +2,9 @@ const Post = require('../models/Post');
 
 module.exports = {
   async index(req, res) {
+    const posts = await Post.find().sort('-createdAt');
 
+    return res.status(200).json(posts);
   },
 
   async store(req, res) {
